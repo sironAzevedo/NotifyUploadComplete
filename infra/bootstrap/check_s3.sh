5a119558e3ce8bfee6550350b1f8f22f4c1b821c
sironAzevedo <sirondba@gmail.com>
2025-08-19 13:37:10 -0300

    Update check_s3.sh

--- infra/bootstrap/check_s3.sh
+++ infra/bootstrap/check_s3.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
 BUCKET_NAME=$1
 
 if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
-  echo "{\"exists\": \"true\"}"
+  echo "{\"exists\": \"true\", \"bucket_name\": \"${BUCKET_NAME}\"}"
 else
-  echo "{\"exists\": \"false\"}"
+  echo "{\"exists\": \"false\", \"bucket_name\": \"${BUCKET_NAME}\"}"
 fi
