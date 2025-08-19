ca8c059512a5604992f89a928c7339ed677396c1
sironAzevedo <sirondba@gmail.com>
2025-08-19 03:39:39 -0300

    ajuste 10.1

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -2,7 +2,6 @@ data "external" "lambda_bucket_existing" {
   program = ["bash", "${path.module}/check_s3.sh", local.name_backet_lambda]
 }
 
-
 data "external" "tfstate_bucket_existing" {
   program = ["bash", "${path.module}/check_s3.sh", local.bucket_name_tfstate]
 }
@@ -23,7 +22,7 @@ resource "aws_s3_bucket" "tfstate" {
 # Bloquear acesso público lambda_bucket
 resource "aws_s3_bucket_public_access_block" "lambda_bucket_block" {
   count                   = data.external.lambda_bucket_existing.result.exists == "false" ? 1 : 0
-  bucket                  = aws_s3_bucket.lambda_bucket.id
+  bucket                  = aws_s3_bucket.lambda_bucket[0].id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -33,7 +32,7 @@ resource "aws_s3_bucket_public_access_block" "lambda_bucket_block" {
 # Bloquear acesso público tfstate_bucket
 resource "aws_s3_bucket_public_access_block" "terraform_tfstate_bucket_block" {
   count                   = data.external.tfstate_bucket_existing.result.exists == "false" ? 1 : 0
-  bucket                  = aws_s3_bucket.tfstate.id
+  bucket                  = aws_s3_bucket.tfstate[0].id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -43,7 +42,7 @@ resource "aws_s3_bucket_public_access_block" "terraform_tfstate_bucket_block" {
 # Habilitar versionamento
 resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
   count     = data.external.s3_check.result.exists == "false" ? 1 : 0
-  bucket    = aws_s3_bucket.lambda_bucket.id
+  bucket    = aws_s3_bucket.lambda_bucket[0].id
   versioning_configuration {
     status = "Enabled"
   }
--- infra/bootstrap/outputs.tf
+++ infra/bootstrap/outputs.tf
@@ -1,7 +1,6 @@
 output "lambda_bucket_name" {
   value = coalesce(
     try(aws_s3_bucket.lambda_bucket[0].id, null),
-    try(data.aws_s3_bucket.lambda_bucket_existing.id, null),
     local.name_backet_lambda
   )
 }
